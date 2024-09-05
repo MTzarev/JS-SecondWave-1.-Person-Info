ee9f25da1f32ff7f3174562235ac00cbb325b055
Miroslav Tsarev <mtzarev@gmail.com>
2024-09-05 19:13:48 +0300

    Add files via upload

---
+++ 1. Person Info.js
@@ -0,0 +1,21 @@
+function personInfo(firstName, lastName, age){
+// let person = {
+//     firstName : firstName,
+//     lastName : lastName,
+//     age: age
+// }
+// return person;
+// }
+// console.log(personInfo("Peter", 
+//     "Pan",
+//     "20"))
+
+let person = {};
+person.firstName = firstName,
+person.lastName = lastName,
+person.age = age
+return person;
+}
+console.log(personInfo("Peter", 
+         "Pan",
+        "20"));
